16b09a668c52b99e4de6d6dbf7a2d19a954de689
Julian Hernandez <julianhernandezguerrero28@gmail.com>
2024-08-09 18:53:22 -0500

    uploads

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
 const textArea = document.querySelector(".textArea");
 const mensajes = document.querySelector(".mensajes");
 
-//*La letra "e" es convertida para "enter"
-//*La letra "i" es convertida para "imes"
-//*La letra "a" es convertida para "ai"
-//*La letra "o" es convertida para "ober"
-//*La letra "u" es convertida para "ufat"
-
 function btnBoton1() {
     if (validarTexto(textArea.value)) {
         const textoEncriptado = encriptar(textArea.value);
-        mensajes.value = textoEncriptado;
-        textArea.value = "";
-        mensajes.style.backgroundImage = "none";
-        ig.style.display = "none"
-        ng.style.display = "none"
+        mostrarTextoEncriptado(textoEncriptado);
     }
 }
 
@@ -32,13 +22,8 @@ function encriptar(stringEncriptada) {
 
 function btnBoton2() {
     if (validarTexto(textArea.value)) {
-        const textoEncriptado = desencriptar(textArea.value);
-        mensajes.value = textoEncriptado;
-        textArea.value = "";
-        mensajes.style.backgroundImage = "none";
-        ig.style.display = "none"
-        ng.style.display = "none"
-        
+        const textoDesencriptado = desencriptar(textArea.value);
+        mostrarTextoEncriptado(textoDesencriptado);
     }
 }
 
@@ -58,7 +43,7 @@ function validarTexto(texto) {
     const tieneMayusculas = /[A-Z]/.test(texto);
     const tieneAcentos = /[áéíóúÁÉÍÓÚ]/.test(texto);
     const tieneNumeros = /[0-9]/.test(texto);
-    const tieneCaracteresE = /[!@#$%^&*(),.?":{}|<>]/.test(texto)
+    const tieneCaracteresE = /[!@#$%^&*(),.?":{}|<>]/.test(texto);
     if (tieneMayusculas || tieneAcentos || tieneNumeros || tieneCaracteresE) {
         alert("Por favor, ingrese solo letras minúsculas y sin acentos.");
         return false;
@@ -67,17 +52,29 @@ function validarTexto(texto) {
     return true;
 }
 
-    function copiarTexto() {
-        const mensajesTextArea = document.querySelector('.mensajes');
-        mensajesTextArea.select(); 
-        try {
-            const exitoso = document.execCommand("copy");
-            if (exitoso) {
-                alert("Texto copiado al portapapeles!");
-            } else {
-                alert("No se pudo copiar el texto.");
-            }
-        } catch (mistake) {
-            alert("Error al copiar el texto: ", mistake);
+function copiarTexto() {
+    const mensajesText = mensajes.textContent;
+    const tempTextArea = document.createElement("textarea");
+    tempTextArea.value = mensajesText;
+    document.body.appendChild(tempTextArea);
+    tempTextArea.select();
+    try {
+        const exitoso = document.execCommand("copy");
+        if (exitoso) {
+            alert("Texto copiado al portapapeles!");
+        } else {
+            alert("No se pudo copiar el texto.");
         }
+    } catch (mistake) {
+        alert("Error al copiar el texto: ", mistake);
     }
+    document.body.removeChild(tempTextArea);
+}
+
+function mostrarTextoEncriptado(texto) {
+    mensajes.textContent = texto;
+    mensajes.style.backgroundImage = "none";
+    ig.style.display = "none";
+    ng.style.display = "none";
+    
+}
